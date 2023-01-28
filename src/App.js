65200f292dcef9743fb937a0418c19dc67aa7d06
komatic1 <111364540+komatic1@users.noreply.github.com>
2023-01-28 13:00:08 +0200

    вариант из курса

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
 import React, { useState } from "react";
-import "./App.css";
-
-import UserList from "./components/Users/UserList/UserList";
-import UserInput from "./components/Users/UserInput/UserInput";
+import CreateUser from "./components/Users/CreateUser";
+import UserList from "./components/Users/UserList";
 
 const App = () => {
-  const [users, setUsers] = useState([
-    { name: "Вася", age: 10, id: "u1" },
-    { name: "Пупка", age: 11, id: "u2" },
-    { name: "Пупа", age: 12, id: "u3" },
-    { name: "Пупа", age: 13, id: "u4" },
-  ]);
+  const [userList, setUserList] = useState([]);
 
-  const addUserHandler = (inputTextName, inputTextAge) => {
-    setUsers((prevUsers) => {
-      const updatedUsers = [...prevUsers];
-      updatedUsers.unshift({
-        name: inputTextName, // нужно новое имя
-        age: inputTextAge, // и возраст
-        id: Math.random().toString(),
-      });
-      return updatedUsers;
+  const createUserHandler = (name, age) => {
+    setUserList((previousUserList) => {
+      return [
+        ...previousUserList,
+        { name: name, age: age, id: Math.random().toString() },
+      ];
     });
   };
-
-  let content = (
-    <p style={{ textAlign: "center" }}>Пользователей не найдено! Добавить?</p>
-  );
-
-  if (users.length > 0) {
-    content = <UserList items={users} />;
-  }
-
   return (
     <div>
-      <section id="user-form">
-        <UserInput onAddTask={addUserHandler} />
-      </section>
-      <section id="users">{content}</section>
+      <CreateUser onCreateUser={createUserHandler} />
+      <UserList users={userList} />
     </div>
   );
 };
--- src/components/Users/User/User.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./User.css";
-
-const User = (props) => {
-  return <li className="user">{props.children}</li>;
-};
-
-export default User;
